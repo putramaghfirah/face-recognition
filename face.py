9139b298919b5f5ad7cc810ab5bc8c0166d2b732
putramaghfirah <putra.maghfirah@gmail.com>
2020-12-17 21:07:42 +0700

    done

--- face.py
+++ face.py
@@ -1,76 +1,97 @@
+# mengimport library yang digunakan
 import face_recognition
+import copy
 from pathlib import Path
 from PIL import Image
 
+# membuat direktori out jika belum ada
+out = Path('out')
+# exist_ok agar jika sudah ada foldernya maka tidak perlu dibuat lagi
+out.mkdir(parents=True, exist_ok=True)
+
+# mengambil tiap foto yang ada di dalam direktori img
 paths = [path for path in Path('img').glob('*.jpg')]
 
+
+# kernel untuk blur
 blur_kernel = [
-    [1/16, 1/8, 1/16],
-    [1/8, 1/4, 1/8],
-    [1/16, 1/8, 1/16]
+    [1/25, 1/25, 1/25, 1/25, 1/25],
+    [1/25, 1/25, 1/25, 1/25, 1/25],
+    [1/25, 1/25, 1/25, 1/25, 1/25],
+    [1/25, 1/25, 1/25, 1/25, 1/25],
+    [1/25, 1/25, 1/25, 1/25, 1/25]
 ]
 
+weight = 0.01
+
+# kernel untuk sharp
 sharpen_kernel = [
-    [0, -1, 0],
-    [-1, 5, -1],
-    [0, -1, 0]
+    [0, -1 * weight, 0, ],
+    [-1 * weight, 4*weight + 1, -1 * weight],
+    [0, -1 * weight, 0, ]
+]
+
+# black kernel
+black_kernel = [
+    [0.05, 0.05, 0.05],
+    [0.05, 0.05, 0.05],
+    [0.05, 0.05, 0.05]
 ]
 
 
-# def blur(image, x, y):
-#     for i in range(3):
-#         total = 0
-#         for vertikal in range(-1, 2):
-#             for horizontal in range(-1, 2):
-#                 total += (image[vertikal+y][horizontal+x][i] *
-#                           blur_kernel[vertikal+1][horizontal+1])
-#         image[y][x][i] = total
-#     return image
-
-
-# def sharpen(image, x, y):
-#     for i in range(3):
-#         total = 0
-#         for vertikal in range(-1, 2):
-#             for horizontal in range(-1, 2):
-#                 total += (image[vertikal+y][horizontal+x][i] *
-#                           sharpen_kernel[vertikal+1][horizontal+1])
-#         image[y][x][i] = total
-#     return image
-
-def blur(image, x, y):
-    skala = 3
-    padding = int(skala/2)
-    for i in range(3):
-        total = 0
-        for vertikal in range(0-padding, padding+1):
-            for horizontal in range(0-padding, padding+1):
-                total += image[vertikal+y][horizontal+x][i]
-        mean = total/(skala*skala)
-        # mean = (int(image[y + 1][x - 1][i]) +
-        #         int(image[y + 1][x][i]) +
-        #         int(image[y + 1][x + 1][i]) +
-        #         int(image[y][x - 1][i]) +
-        #         int(image[y][x][i]) +
-        #         int(image[y][x + 1][i]) +
-        #         int(image[y - 1][x - 1][i]) +
-        #         int(image[y - 1][x][i]) +
-        #         int(image[y - 1][x + 1][i])) / 9
-        image[y][x][i] = mean
-
-
-def filter_image(image, face_location):
+# fungsi untuk menfilter image dengan paramater foto, lokasi wajah, dan kernel dari filter
+def filter_image(image, face_location, kernel: list):
+    # membagi lokasi wajah ke dalam top, right, bottom, left
     top, right, bottom, left = face_location
+    # padding untuk skalanya agar sama dengan kernel
+    padding = int(len(kernel)/2)
+    # melakukan looping untuk vertikal dari top ke bottom
     for y in range(top, bottom):
+        # melakukan looping untuk horizontal dari left ke right
         for x in range(left, right):
-            image = blur(image, x, y)
+            # melakukan terhadap layer dari sebuah foto, dikarenakan dari tiap foto terdapat 3 layer, yaitu r,g,b
+            for i in range(3):
+                # variabel total untuk menyimpan hasil perkalian kernel dengan pixel-pixel tiap foto
+                total = 0
+                # membuat skala 3x3
+                for vertikal in range(0-padding, padding+1):
+                    for horizontal in range(0-padding, padding+1):
+                        # menjumlahkan total dengan image dikali dengan kernel
+                        total += (image[vertikal+y][horizontal+x][i] *
+                                  kernel[vertikal+padding][horizontal+padding])
+                # hasil dari total dimasukkan ke dalam tiap pixel image
+                image[y][x][i] = total
+    # lalu mengembalikan hasil dari image
     return image
 
 
+# melakukan looping untuk tiap foto yang telah didapat dari direktori/folder img
 for path in paths:
+    print(f"Memfilter foto {path}")
+    # mengambil nama file dari tiap foto lalu memasukkan ke dalam variable file_name
+    file_name = path.name
+    # dari foto yang ada diubah ke dalam numpy array/list
     image = face_recognition.load_image_file(path)
+    # mengcopy tiap foto agar tidak menimpa hasil filternya
+    image_blur = copy.deepcopy(image)
+    image_sharp = copy.deepcopy(image)
+    image_black = copy.deepcopy(image)
+    # mengambil wajah yang ada dari tiap foto
     face_locations = face_recognition.face_locations(image)
+    print(f"Lokasi wajah : {face_locations}")
+    # melakukan filter terhadap wajah yang didapat dari sebuah foto
     for face_location in face_locations:
-        image = filter_image(image, face_location)
-    pil_image = Image.fromarray(image)
-    pil_image.save("out/tes.jpg")
+        # for i in range(5):
+        # melakukan filter blur/sharp/black
+        blur = filter_image(image_blur, face_location, blur_kernel)
+        sharp = filter_image(image_sharp, face_location, sharpen_kernel)
+        black = filter_image(image_black, face_location, black_kernel)
+
+    # menyimpan hasil filter ke dalam pil_image lalu save foto yang telah difilter
+    pil_image_blur = Image.fromarray(blur)
+    pil_image_blur.save(f"out/blurred_{file_name}")
+    pil_image_sharp = Image.fromarray(sharp)
+    pil_image_sharp.save(f"out/sharped_{file_name}")
+    pil_image_black = Image.fromarray(black)
+    pil_image_black.save(f"out/black_{file_name}")
+    print(f"foto {path.name} telah difilter")
